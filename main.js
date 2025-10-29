14cfdbbd6a39dd164517078bc3e12623554906c1
sophiatissot-boop <sophia.tissot@escola.pr.gov.br>
2025-10-29 11:44:55 -0300

    m

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log(avanca)
 avanca.forEach(button =>{
     button.addEventListener('cluck', function(){
     const atual =document.querySelector('.ativo')
-    const proximoPasso = 'passo-' + getAttribute('data-proxima')
+    const proximoPasso = 'passo-' + this.getAttribute('data-proxima')
 
     atual.classList.remove('ativo')
     document.getElementById(proximoPasso).classList.add('ativo')
